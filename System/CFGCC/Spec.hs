9d14911723c1204fc91da0000a6638bf09e0e910
Jason Dusek <jason.dusek@gmail.com>
2010-10-01 21:08:17 -0700

    Rewrite and extension of typeclass based approach.

--- System/CFGCC/Spec.hs
+++ System/CFGCC/Spec.hs
@@ -13,27 +13,35 @@ import Data.Text
 
 
 
-class (Monad m) => StaticUNIXThing thing m where
+data Spec e where
+  Is                        ::  (Entry e) => Name e -> Info e -> Spec e
+  IsNot                     ::  (Entry e) => Name e -> Maybe (Info e) -> Spec e
+
+
+class Entry thing where
   data Name thing           ::  *
-  data Spec thing m         ::  *
+  data Info thing           ::  *
+  check                     ::  Name thing -> Maybe (Info thing) -> IO Bool
+  remove                    ::  Name thing -> IO ()
+  create                    ::  Name thing -> Info thing -> IO ()
 
 
 
 
 data User
-instance StaticUNIXThing User Maybe where
+instance Entry User where
   data Name User             =  Username Text | UID Word16
-  data Spec User Maybe       =  User (Maybe Password)
+  data Info User             =  User (Maybe Password)
                                      (Maybe Word16)
                                      (Maybe (Name Group))
-                                     (Maybe (Name FSEntry))
-                                     (Maybe (Name FSEntry))
+                                     (Maybe (Name FSObject))
+                                     (Maybe (Name FSObject))
 
 
 data Group
-instance StaticUNIXThing Group Maybe where
+instance Entry Group where
   data Name Group            =  Groupname Text | GID Word16
-  data Spec Group Maybe      =  Group (Maybe Password) (Maybe Word16)
+  data Info Group            =  Group (Maybe Password) (Maybe Word16)
 
 
 data Password                =  Literal Text | Hashed Text
@@ -42,14 +50,14 @@ data Password                =  Literal Text | Hashed Text
 
 
 
-data FSEntry
-instance StaticUNIXThing FSEntry Maybe where
-  data Name FSEntry          =  Path Text
-  data Spec FSEntry Maybe    =  File (Maybe ContentsChecked)
+data FSObject
+instance Entry FSObject where
+  data Name FSObject         =  Path Text
+  data Info FSObject         =  File (Maybe ContentsChecked)
                                      (Maybe Mode)
                                      (Maybe Ownership)
                              |  Directory (Maybe Mode) (Maybe Ownership)
-                             |  Link (Maybe (Name FSEntry))
+                             |  Link (Maybe (Name FSObject))
 
              
 data Mode                    =  Mode Ternary Ternary Ternary Ternary -- user
